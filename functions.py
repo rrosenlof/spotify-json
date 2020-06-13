28400cdc9e8d671d583aebea90650121592a8097
ross <rosenlof.ross@gmail.com>
2020-06-13 00:30:20 -0600

    more experimenting with sorting

--- functions.py
+++ functions.py
@@ -28,17 +28,19 @@ def get_data():
         print("Play count: {0}".format(count))
 
 def get_latest():
-    with open("json/latest-6-12-20-short_term.json", encoding="utf8") as json_file:
+    with open("json/latest-6-12-20.json", encoding="utf8") as json_file:
         data = json.load(json_file)
         for p in data['items']:
             print("{},{},{}".format(p['artists'][0]['name'],p['name'] ,p['popularity']))
 
 def get_artwork():
-    with open("json/latest-6-12-20.json", encoding="utf8") as json_file:
+    with open("json/2020-playlist.json", encoding="utf8") as json_file:
         data = json.load(json_file)
         artworks = []
+        # for p in data['items']:
+        #     artworks.append(p['album']['images'][1]['url'])
         for p in data['items']:
-            artworks.append(p['album']['images'][1]['url'])
+            artworks.append(p['track']['album']['images'][1]['url'])
 
         artworks = list(dict.fromkeys(artworks))    
 
@@ -69,7 +71,7 @@ def make_collage(artworks):
         rgb_image.paste(img,(pos_right,pos_down), mask=None)
         counter += 1
 
-    rgb_image.save('collage2.jpg')
+    rgb_image.save('images/collage-2020-1.jpg')
 
 def sort_artwork(artworks):
     listArtColors = []
@@ -78,19 +80,25 @@ def sort_artwork(artworks):
         f = io.BytesIO(fd.read())
         color = ColorThief(f)            
         dominant_color = color.get_color(quality=1)
-        hsv_color = format_color(dominant_color)
+        hsv_color = step(dominant_color[0], dominant_color[1], dominant_color[2], 8)
         dictArtColors = dict(artwork=artwork, color=hsv_color)
         listArtColors.append(dictArtColors)
     
     # listArtColors.sort(key=lambda x: x['color'][1], reverse=True)
-    listArtColors.sort(key=lambda x: x['color'], reverse=True)
+    # listArtColors.sort(key=lambda x: x['color'], reverse=True)
+    listArtColors.sort(key=lambda x: x['color'][1])
 
-    print(listArtColors)
     return listArtColors
 
 def format_color(rgb_color):
-    # return colorsys.rgb_to_hsv(rgb_color[0], rgb_color[1], rgb_color[2])
-    return math.sqrt(.241*math.pow(rgb_color[0],2) + .691*math.pow(rgb_color[1],2) + .068*math.pow(rgb_color[2],2))
+    return colorsys.rgb_to_hsv(rgb_color[0], rgb_color[1], rgb_color[2])
+    # return math.sqrt(.241*math.pow(rgb_color[0],2) + .691*math.pow(rgb_color[1],2) + .068*math.pow(rgb_color[2],2))
 
+def step(r,g,b, repetitions=1):
+    lum = math.sqrt(.241*r + .691*g + .068*b)
+    h, s, v = colorsys.rgb_to_hsv(r, g, b)
+    h2 = int(h * repetitions)
+    lum2 = int(lum * repetitions)
+    v2 = int(v * repetitions)
 
-
+    return (h2,lum,v2)
