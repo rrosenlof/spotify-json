66fe8c543df2a429197dd9a96f5baa1f5bf96011
Ross Rosenlof <rrosenlo@byu.edu>
2020-06-30 16:05:32 -0600

    creds work

--- simple-auth.py
+++ simple-auth.py
@@ -1,6 +1,5 @@
 import sys
 import spotipy
-# import spotipy.util as util
 from spotipy.oauth2 import SpotifyClientCredentials
 
 import configparser
@@ -14,12 +13,28 @@ print(client_id)
 client_credentials_manager = SpotifyClientCredentials(client_id,client_secret)
 sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
-playlists = sp.user_playlists('ross.rosenlof')
-while playlists:
-    for i, playlist in enumerate(playlists['items']):
-        print("%4d %s %s" % (i + 1 + playlists['offset'], playlist['uri'],  playlist['name']))
-    if playlists['next']:
-        playlists = sp.next(playlists)
+def show_tracks(tracks):
+    for i, item in enumerate(tracks['items']):
+        track = item['track']
+        print("   %d %32.32s %s" % (i, track['artists'][0]['name'],
+            track['name']))
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        username = sys.argv[1]
     else:
-        playlists = None
-print(playlists)
+        print('need username')
+        sys.exit()
+    
+    playlists = sp.user_playlists(username)
+    for playlist in playlists['items']:
+        print()
+        print(playlist['name'])
+        print ('  total tracks', playlist['tracks']['total'])
+        results = sp.playlist(playlist['id'],
+            fields="tracks,next")
+        tracks = results['tracks']
+        show_tracks(tracks)
+        while tracks['next']:
+            tracks = sp.next(tracks)
+            show_tracks(tracks)
