1108465b4f02a618183400cb2c2f195f44809d0e
ross <rosenlof.ross@gmail.com>
2020-07-01 23:20:43 -0600

    starting to use spotipy

--- spotipy-app/app/routes.py
+++ spotipy-app/app/routes.py
@@ -1,27 +1,27 @@
 from app import app
-from flask import render_template, flash, redirect
+from flask import render_template, flash, redirect, session, url_for
 from app.forms import UsernameForm
+from auth import show_playlists
 
 @app.route('/')
 @app.route('/index')
 def index():
-    user = {'username': 'ross.rosenlof'}
-    posts = [
-        {
-            'author': {'username': 'John'},
-            'body': 'Beautiful day in Portland!'
-        },
-        {
-            'author': {'username': 'Susan'},
-            'body': 'The Avengers movie was so cool!'
-        }
-    ]
-    return render_template('index.html', title='Home', user=user, posts=posts)
+    if 'username' in session:
+        user = session['username']
+        playlists = show_playlists(user)
+        return render_template('index.html', user=user, playlists=playlists)
+    else:
+        return render_template('index.html')
 
 @app.route('/username', methods=['GET', 'POST'])
 def username():
    form = UsernameForm()
    if form.validate_on_submit():
-       flash('username: {}'.format(form.username.data))
+       session['username'] = form.username.data
        return redirect('/index')
    return render_template('username.html', title='Sign In', form=form)
+
+@app.route('/logout')
+def logout():
+   session.pop('username', None)
+   return redirect(url_for('index'))
